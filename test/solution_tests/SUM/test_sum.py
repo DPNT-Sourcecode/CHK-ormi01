8fa756c2a9d3f811a55752eb424ac33ec75780e6
sbx_user1051 <sbx_user1051@169.254.7.49>
2023-12-08 11:20:50 +0000

    Fri Dec 08 11:20:50 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,9 @@
 from solutions.SUM import sum_solution
+import unittest
 
-
-class TestSum():
-    def test_sum(self):
+class TestSum(unittest.TestCase):
+    def test_sum_with_valid_numbers(self):
         assert sum_solution.compute(1, 2) == 3
+    def test_sum_with_invalid_numbers(self):
+        self.assertRaises(ValueError, sum_solution.compute(-1,2))
+
