4200d66bee951e134cbcd8ebefa00f037e2e6b1d
sbx_user1051 <sbx_user1051@169.254.7.49>
2023-12-08 11:39:47 +0000

    Fri Dec 08 11:39:47 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,9 +2,32 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
+    # validate
     if not validate(skus):
         return -1
-    return 1
+    
+    # dictionary of prices containing offers too
+    price = {}
+    price["A"]["offer"][3] = 130 
+    price["A"]["price"] = 50
+    price["B"]["offer"][2] = 45
+    price["B"]["price"] = 30
+    price["C"]["price"] = 20
+    price["D"]["price"] = 15
+
+    # create a dictionary to represent basket
+    basket = {}
+    for item in skus:
+        if item in basket:
+            basket[item] = 0
+        else:
+            basket[item] += 1
+
+    # now want to apply offers
+    for item in basket:
+        # see if there is enough offers 
+
+
 
 def validate(skus: str) -> bool:
     return True
