600049ea82bf778aae3e889e47e27ed28aa44f11
sbx_user1051 <sbx_user1051@169.254.7.49>
2023-12-08 11:23:50 +0000

    Fri Dec 08 11:23:50 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,9 +1,16 @@
 from solutions.SUM import sum_solution
-import unittest
+import pytest
 
-class TestSum(unittest.TestCase):
+class TestSum():
     def test_sum_with_valid_numbers(self):
         assert sum_solution.compute(1, 2) == 3
-    def test_sum_with_invalid_numbers(self):
-        self.assertRaises(ValueError, sum_solution.compute(-1,2))
+    def test_sum_with_invalid_first(self):
+        with pytest.raises(ValueError) as value_error:
+            sum_solution.compute(-1, 2)
+    def test_sum_with_invalid_second(self):
+        with pytest.raises(ValueError) as value_error:
+            sum_solution.compute(2, -1)
+    def test_sum_with_invalid_both(self):
+        with pytest.raises(ValueError) as value_error:
+            sum_solution.compute(-1, -1)
 
