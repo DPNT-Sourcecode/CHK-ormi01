4caf831d868a85aa94dec8424e16d3d0bc228228
sbx_user1051 <sbx_user1051@169.254.7.49>
2023-12-08 11:17:50 +0000

    Fri Dec 08 11:17:50 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,9 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def compute(x: int, y: int) -> int:
+    if valid(x, y):
+        return x + y
+    else:
+        raise ValueError("Arguments provided must be between 0 and 100")
+
+def valid(x: int, y: int) -> bool:
+    return (x >= 0 and x <= 100) and (y >= 0 and y <= 100)
