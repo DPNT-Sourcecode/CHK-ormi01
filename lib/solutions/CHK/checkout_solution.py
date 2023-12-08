4f7acaa13a472697cf7a4fbd310fa7970bd0716d
sbx_user1051 <sbx_user1051@169.254.7.49>
2023-12-08 11:33:47 +0000

    Fri Dec 08 11:33:47 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,10 @@
 
-
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    if not validate(skus):
+        return -1
+    return 1
+
+def validate(skus: str) -> bool:
+    return True
