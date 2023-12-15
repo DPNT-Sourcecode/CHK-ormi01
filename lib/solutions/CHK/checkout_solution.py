e28e1167a5876bd8ca228fb8f7ecfcaa9dbfdd36
sbx_user1051 <sbx_user1051@169.254.14.141>
2023-12-15 10:54:09 +0000

    Fri Dec 15 10:54:09 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,14 +7,14 @@ def checkout(skus: str) -> int:
     offers: dict = {"A": [(5, 200), (3, 130)], "E": [(2, 0, "B")], "B": [(2, 45)], "F": [(3, 20)]}
 
     # initialise basket and the total price
-    basket: dict = {sku: 0 for sku in prices}
+    basket: dict = sorted({sku: 0 for sku in prices}, key=lambda sku: (not any(len(offer) == 3 for offer in offers.get(sku, [])), sku not in offers))
     total_price: int = 0
 
     # count items in basket
     for sku in skus:
         if sku not in prices:
             return -1 # invalid input
-        basket[sku] = 1 if sku not in basket else basket[sku] + 1
+        basket[sku] += 1
 
     # apply offers here
     for sku, amount in basket.items():
@@ -31,6 +31,7 @@ def checkout(skus: str) -> int:
         total_price += amount * prices[sku]
 
     return total_price
+
 
 
 
