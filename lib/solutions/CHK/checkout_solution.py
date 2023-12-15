b88eb9fe949932f2aa1b710d21fa939b6b57ee3f
sbx_user1051 <sbx_user1051@169.254.14.141>
2023-12-15 10:45:09 +0000

    Fri Dec 15 10:45:09 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,11 +3,11 @@
 # skus = unicode string
 def checkout(skus: str) -> int:
     # define prices and offers
-    prices: dict = {"A": 50, "E": 40, "B": 30, "C": 20, "D": 15, "F": 10}
+    prices: dict = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
     offers: dict = {"A": [(5, 200), (3, 130)], "E": [(2, 0, "B")], "B": [(2, 45)], "F": [(3, 20)]}
 
     # initialise basket and the total price
-    basket: dict = {sku: 0 for sku in prices}
+    basket: dict = {}
     total_price: int = 0
 
     # count items in basket
@@ -22,7 +22,7 @@ def checkout(skus: str) -> int:
             for offer in sorted(offers[sku], reverse=True):
                 if len(offer) == 3:  # special offer "buy x get y free"
                     offer_amount, _, free_sku = offer
-                    while amount >= offer_amount and basket.get(free_sku, 0) > 0:
+                    while amount >= offer_amount and free_sku in basket and basket[free_sku] > 0:
                         amount -= offer_amount
                         basket[free_sku] -= 1
                 else:  # regular offer "buy x for y price"
@@ -33,3 +33,4 @@ def checkout(skus: str) -> int:
         total_price += amount * prices[sku]
 
     return total_price
+
